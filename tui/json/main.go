67cdfce305b719682de4296cb9710889a42528b9
Stanislav (Stas) Katkov <git@skatkov.com>
2025-02-09 23:00:44 +0100

    adding root/tui and first prototypes of json

--- tui/json/main.go
+++ tui/json/main.go
@@ -1,41 +1,70 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"log"
+	"fmt"
+	"os"
 
-	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func main() {
-	var json string
-	err := huh.NewForm(
-		huh.NewGroup(
-			huh.NewText().
-				Title("Unformatted JSON").
-				EditorExtension("json").
-				Placeholder("Paste JSON here or open editor with Ctrl+E").
-				Value(&json),
-		),
-
-		huh.NewGroup(huh.NewNote().Height(20).Title("Formatted JSON").
-			DescriptionFunc(func() string {
-				fmd := formatJSON(json)
-				return fmd
-			}, &json)),
-	).WithLayout(huh.LayoutColumns(2)).
-		Run()
-
-	if err != nil {
-		log.Fatal(err)
-	}
+type model struct {
+	content  string
+	viewport viewport.Model
+	showHelp bool
+	ready    bool
 }
 
-func formatJSON(input string) string {
-	var prettyJSON bytes.Buffer
-	if err := json.Indent(&prettyJSON, []byte(input), "", "  "); err != nil {
-		return input // Return original if not valid JSON
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		if !m.ready {
+			// Since this program is using the full size of the viewport we
+			// need to wait until we've received the window dimensions before
+			// we can initialize the viewport. The initial dimensions come in
+			// quickly, though asynchronously, which is why we wait for them
+			// here.
+			m.viewport = viewport.New(msg.Width, msg.Height)
+			m.viewport.SetContent(m.content)
+			m.ready = true
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height
+		}
 	}
-	return prettyJSON.String()
+
+	// Handle keyboard and mouse events in the viewport
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	if !m.ready {
+		return "\n  Initializing..."
+	}
+
+	return fmt.Sprintf("%s", m.viewport.View())
+}
+
+func main() {
+	p := tea.NewProgram(model{content: "Hello, World!"}, tea.WithAltScreen(), tea.WithMouseCellMotion())
+	if err := p.Start(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
