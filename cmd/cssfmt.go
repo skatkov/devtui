4851558e3b83fc0919f6ca8998a4d26183973c31
Stanislav (Stas) Katkov <git@skatkov.com>
2025-03-31 23:54:35 +0200

    feat: adding cssfmt cli

---
+++ cmd/cssfmt.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"log"
+	"os"
+
+	"github.com/client9/csstool"
+	"github.com/spf13/cobra"
+)
+
+// ---
+// Example usage:
+// go run . cssfmt < testdata/bootstrap.min.css
+// ---
+
+var cssfmtCmd = &cobra.Command{
+	Use:   "cssfmt",
+	Short: "Format CSS files",
+	Long:  "Format CSS files",
+	Run: func(cmd *cobra.Command, args []string) {
+		if flagTab {
+			flagIndent = 1
+		}
+		cssformat := csstool.NewCSSFormat(flagIndent, flagTab, nil)
+		cssformat.AlwaysSemicolon = flagSemicolon
+		err := cssformat.Format(os.Stdin, os.Stdout)
+		if err != nil {
+			log.Printf("ERROR: %s", err)
+		}
+	},
+}
+
+var (
+	flagTab       bool
+	flagIndent    int
+	flagSemicolon bool
+)
+
+func init() {
+	rootCmd.AddCommand(cssfmtCmd)
+	cssfmtCmd.Flags().BoolVarP(&flagTab, "tab", "t", false, "use tabs for indentation")
+	cssfmtCmd.Flags().IntVarP(&flagIndent, "indent", "i", 2, "spaces for indentation")
+	cssfmtCmd.Flags().BoolVarP(&flagSemicolon, "semicolon", "", true, "always end rule with semicolon, even if not needed")
+
+}
