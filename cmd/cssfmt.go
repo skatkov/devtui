a0f48417b8fe2784a23ae2792d197b37c6296ad8
Stanislav (Stas) Katkov <git@skatkov.com>
2025-04-17 01:32:12 +0200

    feat: add --tui flag for cssfmt CLI

--- cmd/cssfmt.go
+++ cmd/cssfmt.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"io"
 	"log"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/client9/csstool"
+	"github.com/skatkov/devtui/internal/ui"
+	"github.com/skatkov/devtui/tui/css"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +18,35 @@ var cssfmtCmd = &cobra.Command{
 	Long:    "Format CSS files",
 	Example: `cssfmt < testdata/bootstrap.min.css`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if flagTui {
+			// Read input CSS
+			input, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				log.Printf("ERROR reading input: %s", err)
+				return
+			}
+
+			// Initialize the TUI
+			common := &ui.CommonModel{
+				Width:  80, // Default width, will be adjusted by the TUI
+				Height: 24, // Default height, will be adjusted by the TUI
+			}
+
+			model := css.NewCSSFormatterModel(common)
+			model.SetContent(string(input))
+
+			p := tea.NewProgram(
+				model,
+				tea.WithAltScreen(),       // Use alternate screen buffer
+				tea.WithMouseCellMotion(), // Enable mouse support
+			)
+
+			if _, err := p.Run(); err != nil {
+				log.Printf("ERROR running TUI: %s", err)
+			}
+			return
+		}
+
 		if flagTab {
 			flagIndent = 1
 		}
@@ -30,6 +63,7 @@ var (
 	flagTab       bool
 	flagIndent    int
 	flagSemicolon bool
+	flagTui       bool
 )
 
 func init() {
@@ -37,4 +71,5 @@ func init() {
 	cssfmtCmd.Flags().BoolVarP(&flagTab, "tab", "t", false, "use tabs for indentation")
 	cssfmtCmd.Flags().IntVarP(&flagIndent, "indent", "i", 2, "spaces for indentation")
 	cssfmtCmd.Flags().BoolVarP(&flagSemicolon, "semicolon", "", true, "always end rule with semicolon, even if not needed")
+	cssfmtCmd.Flags().BoolVarP(&flagTui, "tui", "", false, "present result in a TUI")
 }
