707e7f10421e21898235b7502bfc3f2ea964f953
Stanislav (Stas) Katkov <git@skatkov.com>
2025-04-17 01:44:32 +0200

    feat: add --tui flag for xmlfmt CLI

--- cmd/xmlfmt.go
+++ cmd/xmlfmt.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/go-xmlfmt/xmlfmt"
+	"github.com/skatkov/devtui/internal/ui"
+	"github.com/skatkov/devtui/tui/xml"
 	"github.com/spf13/cobra"
 )
 
@@ -25,6 +28,22 @@ var xmlfmtCmd = &cobra.Command{
 			return
 		}
 
+		if useTUI {
+			common := &ui.CommonModel{
+				Width:  0, // Will be set by tea.WindowSizeMsg
+				Height: 0,
+			}
+
+			model := xml.NewXMLFormatterModel(common)
+			model.SetContent(string(data))
+
+			p := tea.NewProgram(model, tea.WithAltScreen())
+			if _, err := p.Run(); err != nil {
+				log.Printf("ERROR: %s", err)
+			}
+			return
+		}
+
 		// Process the XML
 		result := xmlfmt.FormatXML(string(data),
 			xmlPrefix, xmlIndent, xmlNested)
@@ -41,6 +60,7 @@ var (
 	xmlPrefix string
 	xmlIndent string
 	xmlNested bool
+	useTUI    bool
 )
 
 func init() {
@@ -48,4 +68,5 @@ func init() {
 	xmlfmtCmd.Flags().StringVarP(&xmlPrefix, "prefix", "p", "", "Each element begins on a new line and this prefix")
 	xmlfmtCmd.Flags().StringVarP(&xmlIndent, "indent", "i", "  ", "Indent string for nested elements")
 	xmlfmtCmd.Flags().BoolVarP(&xmlNested, "nested", "n", false, "Nested tags in comments")
+	xmlfmtCmd.Flags().BoolVarP(&useTUI, "tui", "t", false, "Enable TUI mode")
 }
