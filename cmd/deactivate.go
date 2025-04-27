f7bb14cd1feb381feaf77e04322ce8fc14a10c94
Stanislav (Stas) Katkov <git@skatkov.com>
2025-04-27 02:54:52 +0200

    improved formatting of licensing keys

--- cmd/deactivate.go
+++ cmd/deactivate.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
 
+	polargo "github.com/polarsource/polar-go"
+	"github.com/polarsource/polar-go/models/components"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +16,21 @@ var deactivateCmd = &cobra.Command{
 	Long:    "Deactivate a license",
 	Example: "devtui deactivate",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Hey!")
+		ctx := context.Background()
+
+		s := polargo.New()
+
+		res, err := s.CustomerPortal.LicenseKeys.Deactivate(ctx, components.LicenseKeyDeactivate{
+			Key:            "DEVTUI-2CA57A34-E191-4290-A394-7B107BCA1036",
+			OrganizationID: "afde3142-5d70-42e3-8214-71c5bbc04e6f",
+			ActivationID:   "14c674ae-2e18-4997-9326-574b2c1cb280",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if res != nil {
+			fmt.Println("Deactivation completed successfully")
+		}
 		return nil
 	},
 }
--- cmd/validate.go
+++ cmd/validate.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
 
+	polargo "github.com/polarsource/polar-go"
+	"github.com/polarsource/polar-go/models/components"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +16,34 @@ var validateCmd = &cobra.Command{
 	Long:    "Validate a license",
 	Example: "devtui validate",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Hey!")
+		ctx := context.Background()
+
+		s := polargo.New()
+
+		res, err := s.CustomerPortal.LicenseKeys.Validate(ctx, components.LicenseKeyValidate{
+			Key:            "DEVTUI-2CA57A34-E191-4290-A394-7B107BCA1036",
+			OrganizationID: "afde3142-5d70-42e3-8214-71c5bbc04e6f",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if res.ValidatedLicenseKey != nil {
+			fmt.Printf("ID: %s\n", res.ValidatedLicenseKey.ID)
+			fmt.Printf("Organization ID: %s\n", res.ValidatedLicenseKey.OrganizationID)
+			fmt.Printf("Customer ID: %s\n", res.ValidatedLicenseKey.CustomerID)
+			fmt.Printf("Customer: %+v\n", res.ValidatedLicenseKey.Customer)
+			fmt.Printf("Benefit ID: %s\n", res.ValidatedLicenseKey.BenefitID)
+			fmt.Printf("Key: %s\n", res.ValidatedLicenseKey.Key)
+			fmt.Printf("Display Key: %s\n", res.ValidatedLicenseKey.DisplayKey)
+			fmt.Printf("Status: %s\n", res.ValidatedLicenseKey.Status)
+			fmt.Printf("Limit Activations: %v\n", res.ValidatedLicenseKey.LimitActivations)
+			fmt.Printf("Usage: %d\n", res.ValidatedLicenseKey.Usage)
+			fmt.Printf("Limit Usage: %v\n", res.ValidatedLicenseKey.LimitUsage)
+			fmt.Printf("Validations: %d\n", res.ValidatedLicenseKey.Validations)
+			fmt.Printf("Last Validated At: %v\n", res.ValidatedLicenseKey.LastValidatedAt)
+			fmt.Printf("Expires At: %v\n", res.ValidatedLicenseKey.ExpiresAt)
+			fmt.Printf("Activation: %+v\n", res.ValidatedLicenseKey.Activation)
+		}
 		return nil
 	},
 }
