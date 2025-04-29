f64715956eb482db342597f3d2be39d9b411a0a5
Stanislav (Stas) Katkov <git@skatkov.com>
2025-04-29 02:14:09 +0200

    refactor: cssmin CLI to properly handle errors

--- cmd/cssmin.go
+++ cmd/cssmin.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"log"
 	"os"
 
 	"github.com/client9/csstool"
@@ -13,13 +12,15 @@ var cssminCmd = &cobra.Command{
 	Short:   "Minify CSS files",
 	Long:    "Minify CSS files",
 	Example: "cssmin < testdata/bootstrap.min.css",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		cssformat := csstool.NewCSSFormat(0, false, nil)
 		cssformat.AlwaysSemicolon = false
 		err := cssformat.Format(os.Stdin, os.Stdout)
 		if err != nil {
-			log.Printf("ERROR: %s", err)
+			return err
 		}
+
+		return nil
 	},
 }
 
