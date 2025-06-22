451a012dcfe595ef07af8ff0f57a28c233136c81
Stanislav (Stas) Katkov <contact@skatkov.com>
2025-06-23 00:27:16 +0200

    feat: include FENG library in CLI interface

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,20 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/fang"
 	"github.com/skatkov/devtui/tui/root"
 	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "devtui",
 	Short: "A Swiss Army knife for developers",
 	Long: `devtui is a collection of small developer apps that help with day to day work.
-	It includes tools like hash generator, unix timestamp converter, and number base converter and multiple others.`,
+It includes tools like hash generator, unix timestamp converter, and number base converter and multiple others.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		p := tea.NewProgram(root.RootScreen(), tea.WithAltScreen())
 		if _, err := p.Run(); err != nil {
@@ -26,9 +27,8 @@ var rootCmd = &cobra.Command{
 var flagTUI bool
 
 func Execute() {
-	err := rootCmd.Execute()
+	err := fang.Execute(context.Background(), rootCmd)
 	if err != nil {
-		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 }
